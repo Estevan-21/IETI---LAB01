f7c26bc5021888cfda9b3bbcec46ee4a5ba51a6e
Brayam <bvargas@e-learning.com.co>
2019-08-30 16:53:30 -0500

    V 1.1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Todo} from './Todo'
+import {TodoList} from './TodoList'
 
 function App() {
+  const todos = [{text:"Learn React", priority:5, dueDate: new Date() },
+          {text:"Learn about APIs", priority:4, dueDate: new Date(2018,8,30) },
+          {text:"write TODO App", priority:3, dueDate: new Date(2018,9,30) }];
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <TodoList todoList={todos}/>
         </p>
         <a
           className="App-link"
@@ -16,11 +21,11 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
         </a>
       </header>
     </div>
   );
 }
+
 
 export default App;
